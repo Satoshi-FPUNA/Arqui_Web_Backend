a018a6c283433dae745c07a4529c46e18c3c0dfc
Satoshi <satoshinakamori3@fpuna.edu.py>
2025-10-30 17:43:33 -0300

    Creacion de tablas de los puntos y sus usos

--- app/schemas.py
+++ app/schemas.py
@@ -15,3 +15,26 @@ class ClientCreate(BaseModel):
 class ClientUpdate(BaseModel):
     telefono: Optional[str] = None
     email: Optional[EmailStr] = None
+
+class RuleCreate(BaseModel):
+    limite_inferior: Optional[int] = None
+    limite_superior: Optional[int] = None
+    equivalencia_monto: int
+
+class ExpirationParamCreate(BaseModel):
+    fecha_inicio_validez: Optional[date] = None
+    fecha_fin_validez: Optional[date] = None
+    dias_duracion: Optional[int] = None
+
+class ConceptCreate(BaseModel):
+    descripcion: str
+    puntos_requeridos: int
+
+class AssignPointsRequest(BaseModel):
+    cliente_id: int
+    monto_operacion: int
+
+class UsePointsRequest(BaseModel):
+    cliente_id: int
+    concepto_id: int
+    puntos: int
